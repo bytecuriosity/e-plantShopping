6426782bbcc70a1d0a625d626e8718b4f7a2defd
Petr Tolshin <tolshinpetr@proton.me>
2026-07-19 10:46:04 +0200

    Added the number on the cart without functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     // State to show cart
     const [showCart, setShowCart] = useState(false);
     // State to control the visibility of the About Us page
-    const [showPlants, setShowPlants] = useState(false); 
+    const [showPlants, setShowPlants] = useState(false);
     // Boolean, is product in the cart
     const [addedToCart, setAddedToCart] = useState({});
     // Dispatch variable
     const dispatch = useDispatch();
-    // CartItems
-    const CartItems = useSelector(state => state.cart.items);
-    // Plants array
+    // Cart Items
+    const CartItems = useSelector((state) => state.cart.items);
+    // Cart
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -223,6 +223,7 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -244,48 +245,45 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
-    {/* Calculate total quantity of items */}
-    const calculateTotalQuantity = () => {
-        if (!CartItems) return 0;
-        
-        let totalQuantity = 0;
-        for (const item of CartItems) {
-            totalQuantity =+ item.quantity;
-        }
-
-        return totalQuantity;
+    {/* Style for the amount of items in cart */ }
+    const styleCart = {
+        fill: 'white',
+        fontSize: '80px'
     }
 
-    {/* Add product to cart and change the button style */}
+    {/* Calculate total quantity of items */ }
+    const calculateTotalQuantity = () => { 
+	return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0; };
+
+    {/* Add product to cart and change the button style */ }
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
-
         setAddedToCart((prevState) => ({
             ...prevState,
             [product.name]: true,
         }));
     };
 
-    {/* Change to home page*/}
+    {/* Change to home page*/ }
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
     };
 
-    {/* Swap products page with cart */}
+    {/* Swap products page with cart */ }
     const handleCartClick = (e) => {
         e.preventDefault();
         setShowCart(true); // Set showCart to true when cart icon is clicked
     };
 
-    {/*  */}
+    {/*  */ }
     const handlePlantsClick = (e) => {
         e.preventDefault();
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
         setShowCart(false); // Hide the cart when navigating to About Us
     };
 
-    {/* Swap cart with products */}
+    {/* Swap cart with products */ }
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
@@ -308,7 +306,21 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                            <rect width="156" height="156" fill="none"></rect>
+                            <circle cx="80" cy="216" r="12"></circle>
+                            <circle cx="184" cy="216" r="12"></circle>
+                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                            {/* Quantity being shown in the cart */}
+                            <text
+                                x='130'
+                                y='120'
+                                textAnchor='middle'
+                                dominantBaseline='central'
+                                fill='white'
+                                style={styleCart}>0</text>
+                        </svg></h1></a></div>
                 </div>
             </div>
             {/* Product cards from redux cart OR cart */}
@@ -317,7 +329,7 @@ function ProductList({ onHomeClick }) {
                     {plantsArray.map((category, index) => (
                         <div key={index}>
                             <h1>
-                                <div>{category.category}</div>
+                                <div className="category-centered">{category.category}</div>
                             </h1>
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
